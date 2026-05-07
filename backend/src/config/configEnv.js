9cd45d3e9bb0d9be8c4dafa147b341268a6c874c
BarbaraaIC <antonia.inostroza711@gmail.com~>
2026-05-06 23:49:30 -0400

    dockers por separado

---
+++ backend/src/config/configEnv.js
@@ -0,0 +1,6 @@
+// configEnv.js
+export const HOST = process.env.DB_HOST || 'localhost';
+export const DB_PORT = process.env.DB_PORT || 5432;
+export const DB_USERNAME = process.env.DB_USER || 'postgres';
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'postgres1234';
+export const DATABASE = process.env.DB_NAME || 'TareaDocker';
